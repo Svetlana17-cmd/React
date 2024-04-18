873dfcd7a8b57adcea4ae5ccb7a22f68c0af4e0c
@Svetlana <130667941+Svetlana17-cmd@users.noreply.github.com>
2024-04-18 11:11:38 +0300

    Add files via upload

---
+++ osa1/unicafe/step 4.jsx
@@ -0,0 +1,62 @@
+
+import { useState } from 'react'
+
+
+
+const Statistics = (props) => {
+	const total = props.good + props.neutral + props.bad;
+	if (total === 0) {
+		return <p>No feedback given</p>
+	}
+	const average = (props.good - props.bad) / total
+	const positive = (props.good / total) * 100
+	return (
+		<div>
+			<p>All: {total}</p>
+			<p>Average: {average}</p>
+			<p>Positive: {positive} %</p>
+		</div>
+	);
+};
+
+
+const App = () => {
+	const [good, setGood] = useState(0)
+	const [neutral, setNeutral] = useState(0)
+	const [bad, setBad] = useState(0)
+
+
+
+	const handleGoodClick = () => {
+		const newGood = good + 1
+		setGood(newGood);
+	}
+	const handleNeutralClick = () => {
+		const newNeutral = neutral + 1
+		setNeutral(newNeutral);
+	}
+	const handleBadClick = () => {
+		const newBad = bad + 1
+		setBad(newBad);
+	}
+
+
+	return (
+		<div>
+			<h1>Unicafe - Asikaspalaute</h1>
+			<button onClick={handleGoodClick}>Good</button>
+			<button onClick={handleNeutralClick}>Neutral</button>
+			<button onClick={handleBadClick}>Bad</button>
+			<h2>Statistics</h2>
+			<Statistics
+
+				good ={good}
+				neutral={neutral}
+				bad={bad}
+			/>
+
+		</div>
+	);
+
+};
+export default App
