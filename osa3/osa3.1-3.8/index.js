227950951bcb76594b327d5df4abfd5de1e50405
@Svetlana <130667941+Svetlana17-cmd@users.noreply.github.com>
2024-10-21 11:10:04 +0300

    file changed@Svetlana17-cmd

--- osa3/osa3.1-3.8/index.js
+++ osa3/osa3.1-3.8/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 const cors = require("cors");
 
 let persons = [
@@ -25,13 +26,12 @@ let persons = [
 	},
 ];
 
-const requestLogger = (request, response, next) => {
-	console.log("Method:", request.method);
-	console.log("Path:", request.path);
-	console.log("Body:", request.body);
-	console.log("---");
-	next();
-};
+morgan.token("body", (req) => JSON.stringify(req.body));
+app.use(
+	morgan(
+		":method :url :status :res[content-length] - :response-time ms :body",
+	),
+);
 
 app.use(express.json());
 app.use(cors());
