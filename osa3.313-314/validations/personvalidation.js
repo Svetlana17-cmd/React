d159560a5fe869da20600be818c9ddd87958d241
Svetlana Andreeva <nik.dan1999@gmail.com>
2024-11-26 11:53:58 +0200

    Tehtaävät 3.13-3.14

---
+++ osa3.313-314/validations/personvalidation.js
@@ -0,0 +1,8 @@
+const { body } = require("express-validator");
+
+const registerValidation = [
+	body("email").isEmail(),
+	body("password").isLength({ min: 5 }),
+	body("fullName").isLength({ min: 3 }),
+	body("avatarUrl").optional().isURL(),
+];
